514344b40d01039b457d7b800264cb165804141c
banyawat <bunyawat.38@gmail.com>
2016-04-18 10:42:42 +0700

    NN fixed inputs bug

--- src/myProject/TestNN.java
+++ src/myProject/TestNN.java
@@ -10,7 +10,7 @@ public class TestNN {
 	static final int INPUT_NUM = 8;
 	static final int HIDDEN_NUM = 9;
 	static final int OUTPUT_NUM = 4;
-	static final double INIT_WEIGHT = .15;
+	static final double INIT_WEIGHT = .5;
 	static final double LEARNING_RATE = .5;
 	public static double[][] weightH = new double[HIDDEN_NUM][INPUT_NUM];
 	public static double[][] weightO = new double[OUTPUT_NUM][HIDDEN_NUM];
@@ -19,13 +19,14 @@ public class TestNN {
 	public static void main(String[] args)
 	{
 		String data;
-		int input[];
+		int sample;
+		double input[];
 		String sampleStr[];
 		
 		double net, totalError, totalErrorHidden;
 		double[] outH = new double[HIDDEN_NUM];
 		double[] outO = new double[OUTPUT_NUM];
-		double[] targetO = {0.2, 0.3, 0.4, 0.5};
+		double[] targetO = {1, 0.1, 0.1, 0.1};
 		double biasL1 = .35, biasL2 = .2;
 		
 		//Assign initial weight to each layers
@@ -61,13 +62,14 @@ public class TestNN {
 				if(data!="null"){
 					data = data.replaceAll("\\[|\\]|\\s", "");
 					sampleStr = data.split(",");
-					input = new int[sampleStr.length];
+					input = new double[sampleStr.length];
 					for(int i=0;i<sampleStr.length;i++){
-						input[i] = Integer.parseInt(sampleStr[i]); //get sample data from each nodes
-						if(input[i]<0)
-							input[i]*=-1;
+						sample = Integer.parseInt(sampleStr[i]); //get sample data from each nodes
+						if(sample<0)
+							sample*=-1;
 						//System.out.print(i+": "+sample[i]+"\t");
-						input[i]/=128; //map number range as 0-1 from 0-128
+						input[i]=(double)sample/128; //map number range as 0-1 from 0-128
+						//System.out.println("Input: "+input[i]);
 					}
 					//do something - Forward Pass
 					for(int i=0;i<HIDDEN_NUM;i++){
@@ -77,7 +79,9 @@ public class TestNN {
 						}
 						net += biasL1;
 						outH[i] = 1/(1+Math.exp(-net));
+						//System.out.println("outH["+i+"]: "+outH[i]);
 					}
+					System.out.println();
 					
 					for(int i=0;i<OUTPUT_NUM;i++){
 						net = 0;
@@ -86,6 +90,7 @@ public class TestNN {
 						}
 						net+=biasL2;
 						outO[i] = 1/(1+Math.exp(-net));
+						//System.out.println("outO["+i+"]: "+outO[i]);
 					}
 					
 					//Calculating total error
@@ -97,10 +102,13 @@ public class TestNN {
 					// NOW BACKWARD PASS START WITH OUTPUT LAYER
 					for(int i=0;i<OUTPUT_NUM;i++){
 						for(int j=0;j<HIDDEN_NUM;j++){
-							weightO[i][j] = weightO[i][j]-LEARNING_RATE*((outO[i]-targetO[i])*(outO[i]*(1-outO[i])*outO[i]));
+							weightO[i][j] = weightO[i][j]-LEARNING_RATE*((outO[i]-targetO[i])*(outO[i]*(1-outO[i])*outH[i]));
+							System.out.println("Output Weight[H"+j+"][O"+i+"]: "+weightO[i][j]);
 						} //adjusting weight for output weight
+						System.out.println();
 					}
 					
+					System.out.println(" ****** HIDDEN LAYER ******");
 					//START ADJUST HIDDEN LAYER
 					for(int i=0;i<HIDDEN_NUM;i++){
 						totalErrorHidden = 0;
@@ -109,8 +117,11 @@ public class TestNN {
 						}
 						for(int j=0;j<INPUT_NUM;j++){
 							weightH[i][j] = weightH[i][j]-LEARNING_RATE*(outH[i]*(1-outH[i])*input[j]);
+							System.out.println("Hidden Weight[I"+j+"][H"+i+"]: "+weightH[i][j]);
 						}
+						System.out.println();
 					} //adjusting weight for each hidden layer weight
+					System.out.println();
 				}
 			}
 		} catch (Exception e) {
